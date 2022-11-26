daab1f0a7261daf9095bcb72969b351167ccc520
BoonyaOnkling <boonyanutch.133@gmail.com>
2022-11-26 11:25:12 +0700

    workshopA

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.status(200).json({
+    Fullname:'Boonyanutch Onkling'
+  })
+});
+
+router.get('/bio',function(req, res, next){
+  res.status(200).json({
+    fullname: "Boonyanutch Onkling",
+    nickname: "Pareploy",
+    hobby: "Play Game",
+    gitusername: "BoonyaOnkling"
+  })
 });
 
 module.exports = router;
