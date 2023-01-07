c13a2c5f736728c7e834c730fee3776f6ce52796
BoonyaOnkling <boonyanutch.133@gmail.com>
2023-01-07 11:45:05 +0700

    WorkshopB

--- models/staff.js
+++ models/staff.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const staffSchema = new Schema({
-    name:  {type: String, required:true, trim:true}, 
-    salary: {type: Number},
-    created: {type: Date, default: Date.now }
+    name:  {type: String, required:true, trim:true},
+    photo: {type: String, default: 'nopic.png'},
   },{collection:"staffs"});
 
   const staff = mongoose.model("staff",staffSchema)
